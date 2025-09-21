777a3ea60fb61da540f7369cc98397f2a43f7452
Maria Eliza <eliza.sads@gmail.com>
2025-09-21 02:42:32 -0300

    fix: adjusted ts importing

--- src/services/LeadsService.ts
+++ src/services/LeadsService.ts
@@ -1,7 +1,7 @@
-import type { Leads } from "../@types/Leads";
+import type { Lead } from "../@types/Leads";
 import { leadsData } from "../data/leads";
 
-export async function fetchLeads(): Promise<Leads[]> {
+export async function fetchLeads(): Promise<Lead[]> {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(leadsData);
